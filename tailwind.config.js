b3b60fe6242ffee06cedb268f0bc5df459b06c92
dashi296 <falcon0296@gmail.com>
2020-03-01 06:00:57 +0900

    tailwindcss導入

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/*
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+module.exports = {
+  theme: {
+    colors: {
+      primary: '#07889B',
+      secondary: '#E37222',
+      accent: '#EEAA7B',
+      gray: '#999',
+      lightgray: '#DDD',
+      facebook: '#3B5998',
+      twitter: '#1DA1F2',
+      github: '#171515',
+      white: '#FFFFFF',
+      black: '#000000'
+    },
+    inset: {
+      '0': 0,
+      auto: 'auto',
+      '1/2': '50%',
+      '1/5': '20%'
+    }
+  },
+  variants: {},
+  plugins: []
+}
