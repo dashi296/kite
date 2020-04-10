8f04313cd5ef9c529f1eaa52d77604772ab9cd81
dashi296 <falcon0296@gmail.com>
2020-04-11 01:11:32 +0900

    feat: コードブロックのファイル名表示を修正

--- static/js/codeTitle.js
+++ static/js/codeTitle.js
@@ -1,11 +1,14 @@
-var list = document.body.getElementsByClassName("highlight");
+const list = document.body.getElementsByClassName("highlight");
+
+const whiteListFileName = ["Dockerfile"]
 
 for(i=0; i <= list.length-1; i++){
-  var code = list[i].firstElementChild.firstElementChild
-  var codeName =  code ? code.className.split(":")[1] : null;
+  const code = list[i].firstElementChild.firstElementChild
+  if(!code) continue
+  const codeName = whiteListFileName.includes(code.dataset.lang) ? code.dataset.lang : code.className.split(":")[1]
 
   if(codeName) {
-    var div = document.createElement('div');
+    const div = document.createElement('div');
     div.textContent = codeName;
     div.classList.add('code-name');
     code.parentNode.insertBefore(div, code);
